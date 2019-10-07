49134fe4b1738ae6e8eed17d3744f602b31aabc5
Guido Kleine <33725136+GuidoKleine@users.noreply.github.com>
2019-10-07 12:22:40 +0200

    commiting several files for test

--- CM-BPM-OJET-WebApplication/src/js/viewModels/taskManager.js
+++ CM-BPM-OJET-WebApplication/src/js/viewModels/taskManager.js
@@ -1,25 +1,42 @@
-define(['ojs/ojcore', 'knockout', 'jquery', "ojs/ojtable", "ojs/ojarraydataprovider", "ojs/ojbutton"],
-  function (oj, ko, $, ArrayDataProvider) {
+define(['knockout', 'jquery', "ojs/ojtable", "ojs/ojbutton"],
+  function (ko, $) {
 
     function TaskManagerViewModel() {
       var self = this;
 
-      // buttons
-      self.acceptButton = ko.observable();
-      self.acceptButton(oj.Translations.getTranslatedString('accept-buttonText'));
-
-      self.correctButton = ko.observable();
-      self.correctButton(oj.Translations.getTranslatedString('correct-buttonText'))
-
       // Mock Data
-      self.mockData = [
+      self.mockData = ko.observableArray([
         { taskId: "1", taskName: "Calculater report", dateTime: "2-10-2019 11:36", errors: "0", warnings: "1", comments: "Check with product owner" }
-
-      ];
+      ]);
 
       self.taskData = new oj.ArrayDataProvider(
         self.mockData, { idAttribute: "taskId" }
       )
+
+      // buttons
+      self.acceptButton = ko.observable();
+      self.acceptButton(oj.Translations.getTranslatedString('Accept'));
+
+      self.correctButton = ko.observable();
+      self.correctButton(oj.Translations.getTranslatedString('correct-buttonText'))
+
+      // enables/disables accept button, not working correctly yet
+      console.log(self.mockData()[0].errors == 0);
+
+      self.needsToBeDisabled = function () {
+        if (self.mockData()[0].errors == 0) {
+          $("#acceptButtonTask").ojButton("disabled", false)
+          }  $("#acceptButtonTask").ojButton("disabled", true);
+      }
+
+      console.log(self.needsToBeDisabled());
+
+      self.needsToBeDisabled();
+
+      self.buttonClick = function () {
+        console.log("Number of errors: " + self.mockData()[0].errors);
+      }
+
     }
     return new TaskManagerViewModel();
   }
